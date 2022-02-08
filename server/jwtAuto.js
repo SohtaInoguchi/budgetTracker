45103296ac7421f1692ef5191dc6ec906876c090
JohnnyDamon <53366209+JohnnyDamon@users.noreply.github.com>
2022-02-08 22:17:57 +0900

    feat: token generator

--- server/jwtAuto.js
+++ server/jwtAuto.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const pool = require('./logindb');
 const bcrypt = require('bcrypt');
+const jwtGenerator = require('../utils/jwtGenerator');
 
 router.post("/register", async (req, res) => {
     try {
@@ -21,7 +22,9 @@ router.post("/register", async (req, res) => {
         const newUser = await pool.query
         ("INSERT INTO users (user_name, user_email, user_password) VALUES ($1, $2, $3) RETURNING *", [name, email, bcryptPassword]); 
 
-        res.json(newUser.rows[0]);
+        const token = jwtGenerator(newUser.rows[0].user_id);
+
+        res.json({ token });
 
     } catch (err) {
         console.error(err);
